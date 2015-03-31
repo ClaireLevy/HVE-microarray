0bbfbaf03555f0a71645676993b463198bdae49f
CLAIRE LEVY <clairel@uw.edu>
2015-03-31 11:19:03 -0700

    removed line that arranges

--- subsetToOverlappingGoTerms.R
+++ subsetToOverlappingGoTerms.R
@@ -4,14 +4,12 @@ subsetToOverlappingGoTerms <- function(df, direction, withDay1) {
       toWrite <- toWrite %>%
          group_by(Term)%>%
          filter(n()==4)%>% # because there are 4 days
-         arrange(Day,Term,PValue)%>%
          ungroup()
    } else {
       toWrite <- df[df$direction == direction & df$Day != 1, ]
       toWrite <- toWrite %>%
          group_by(Term)%>%
          filter(n()==3)%>% # because there are 3 days
-         arrange(Day,Term,PValue)%>%
          ungroup()
    }
 }
