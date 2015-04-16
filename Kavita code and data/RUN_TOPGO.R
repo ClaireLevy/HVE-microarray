38dcc30499b39775c173dd5bb015fe5a54d7e8eb
CLAIRE LEVY <clairel@uw.edu>
2015-04-16 09:20:51 -0700

    added my code for getting a new GOtoGene mapping from biomaRt

--- Kavita code and data/RUN_TOPGO.R
+++ Kavita code and data/RUN_TOPGO.R
@@ -1,12 +1,15 @@
-library(topGO)
 
+setwd("J:/MacLabUsers/Claire/Projects/HVE-microarray/Kavita code and data")
+
+########## KAVITA'S CODE, MY NOTES##############################
+require(topGO)
+setwd("J:/MacLabUsers/Claire/Projects/HVE-microarray/Kavita code and data")
 load("all_genes.Rda") #universe? gene symbols
-load("entrez_annot.Rda") #mapping GO to  (entrez id)Genes
+load("entrez_annot.Rda") #mapping GO to gene symbols
 load("found_genes.Rda") #gene symbols
 load("found_genes_wang.Rda")#gene symbols
 
-#find the overlap between found_genes and all_genes (same length as
-#all_genes!)
+
 found = factor(as.integer(all_genes %in% found_genes))
 
 #assign the names from all_genes to the 1's and 0's in found
@@ -18,7 +21,7 @@ PsampleGOdata <- new("topGOdata",description = "Simple session",
                      allGenes = found,
                      nodeSize = 5,
                      annot = annFUN.GO2genes,
-                     GO2genes = entrez_annot)#why entrez if others are symbol?
+                     GO2genes = entrez_annot)
 
 PresultClassic = runTest(PsampleGOdata,algorithm = 'classic',
                          statistic =  'fisher')
@@ -58,3 +61,39 @@ for (i in 1:nrow(Presults.table)) {
         line = paste(GOid.of.interest,genes,sep = "\t");
         write.table(line,"GenesInGoSCRI.tsv",append = TRUE)
 }
+
+
+######################### MY CODE AND NOTES ################################
+require(biomaRt)
+require(topGO)
+load("all_genes.Rda") #universe? gene symbols
+load("found_genes.Rda") #gene symbols
+load("found_genes_wang.Rda")#gene symbols
+
+
+#which in all_genes overlap with those in found_genes (logical)
+found = factor(as.integer(all_genes %in% found_genes))
+
+#assign the names from all_genes to the 1's and 0's in found
+names(found) = all_genes
+
+#set up mart
+ensembl<-useMart("ensembl",dataset="hsapiens_gene_ensembl")
+
+#get entrez id, go id, strand for found_genes
+annotations<-getBM(attributes=c("hgnc_symbol","entrezgene","go_id","strand"),
+             filters = "hgnc_symbol",values= found_genes,
+             mart = ensembl)
+
+#make a list mapping GO ids and their corresponding symbols (format for topGO)
+GOtoGene<- split(annotations$hgnc_symbol,annotations$go_id)
+GOtoGene<- lapply(GOtoGene, unique)  # to remove duplicates
+
+#now  have a new mapping of GO to symbol,can use topGO
+
+topGOdata <- new("topGOdata",description = "Simple session",
+                     ontology = "BP",
+                     allGenes = found,
+                     nodeSize = 5,
+                     annot = annFUN.GO2genes,
+                     GO2genes = symbolToGO)
